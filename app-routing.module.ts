4d0bf677c5ae94fab9ed7824150fbc9cb7e5c2fc
k-manalo <54648516+k-manalo@users.noreply.github.com>
2020-12-08 00:32:18 -0500

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'wish',
+    loadChildren: () => import('./wish/wish.module').then( m => m.WishPageModule)
+  },
+  {
+    path: 'wish/:id',
+    loadChildren: () => import('./wish/wish.module').then( m => m.WishPageModule)
+  },
+  {
+    path: 'filter',
+    loadChildren: () => import('./filter/filter.module').then( m => m.FilterPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
